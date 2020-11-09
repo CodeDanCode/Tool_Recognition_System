fb6440b6ae91a5c9a5dcfa27f48347ea06209341
Daniel Leach <dleach2018@fau.edu>
2020-11-09 14:22:36 -0500

    updated actionbar color

--- app/src/main/java/com/example/firebaseobjectrecognition/MainActivity.java
+++ app/src/main/java/com/example/firebaseobjectrecognition/MainActivity.java
@@ -30,7 +30,7 @@ import java.util.List;
 
 @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
 public class MainActivity extends AppCompatActivity {
-    
+
     private static final String TAG = "Probability";
 
     private Button btnCapture;
@@ -43,6 +43,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getSupportActionBar().setTitle("HebiSoft");
 
         btnCapture = findViewById(R.id.button);
         imageView = findViewById(R.id.imageView);
